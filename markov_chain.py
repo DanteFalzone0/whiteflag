737e31e578328571e210051ee45a572222784d96
Jack McLovin <adenhandasyde@Adens-MacBook-Pro.local>
2022-05-20 07:27:23 +1000

    hydraulic cylinder

--- markov_chain.py
+++ markov_chain.py
@@ -3,7 +3,11 @@ import random
 class MarkovChain:
   def __init__(self, text, delimeter=" "):
     self._dict = {}
-    tokens = text.split(delimeter)
+    tokens = None
+    if delimeter is not None:
+      tokens = text.split(delimeter)
+    else:
+      tokens = list(text)
 
     # Populate the dict
     for token in tokens:
@@ -14,17 +18,20 @@ class MarkovChain:
 
   # Given a token, return a token that could come after it
   def get_next_token(self, prev_token):
-    if prev_token not in self._dict.keys():
-      # Return a random token
-      rand_key = random.choice(self._dict.keys())
-      return random.choice(self._dict[rand_key])
-    else:
-      return random.choice(self._dict[prev_token])
-
+    try:
+      if prev_token not in self._dict.keys():
+        # Return a random token
+        keys = list(self._dict.keys())
+        rand_key = random.choice(keys)
+        return random.choice(self._dict[rand_key])
+      else:
+        return random.choice(self._dict[prev_token])
+    except IndexError:
+      return "\n"
 
 text = input("Input some text to test the algorithm: ")
 markov_chain = MarkovChain(text)
 token = "The"
-for _ in range(5):
+for _ in range(200):
   print(token, end=" ")
   token = markov_chain.get_next_token(token)
