7b750a7d3ba3eae50b20b405e65f3713c50c9d81
Dante Falzone <48266179+DanteFalzone0@users.noreply.github.com>
2022-05-28 00:06:42 -0500

    indentation and formatting

--- rust/main.rs
+++ rust/main.rs
@@ -8,54 +8,54 @@ use rocket::http::Method;
 use rocket_contrib::serve::StaticFiles;
 use rocket::response::{NamedFile};
 use rocket_cors::{
-AllowedHeaders, AllowedOrigins, Error,
-Cors, CorsOptions
+  AllowedHeaders, AllowedOrigins, Error,
+  Cors, CorsOptions
 };
+
 fn make_cors() -> Cors {
-let allowed_origins = AllowedOrigins::some_exact(&[ // 4.
-"http://localhost:3000",
-"http://127.0.0.1:3000",
-"http://localhost:8000",
-"http://0.0.0.0:8000",
-]);
-CorsOptions { // 5.
-allowed_origins,
-allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(), // 1.
-allowed_headers: AllowedHeaders::some(&[
-"Authorization",
-"Accept",
-"Access-Control-Allow-Origin", // 6.
-]),
-allow_credentials: true,
-..Default::default()
+  let allowed_origins = AllowedOrigins::some_exact(&[ // 4.
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:8000",
+    "http://0.0.0.0:8000",
+  ]);
+  CorsOptions { // 5.
+    allowed_origins,
+    allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(), // 1.
+    allowed_headers: AllowedHeaders::some(&[
+      "Authorization",
+      "Accept",
+      "Access-Control-Allow-Origin", // 6.
+    ]),
+    allow_credentials: true,
+    ..Default::default()
+  }.to_cors().expect("error while building CORS")
 }
-.to_cors()
-.expect("error while building CORS")
-}
+
 #[get("/api/myrocket")]
 fn myrocket() -> String {
-"My 🚀 server".to_string()
+  "My 🚀 server".to_string()
 }
+
 #[get("/<file..>")]
 fn files(file: PathBuf) -> Option<NamedFile> {
-NamedFile::open(Path::new("build/").join(file)).ok()
+  NamedFile::open(Path::new("build/").join(file)).ok()
 }
+
 #[get("/")]
 fn index() -> io::Result<NamedFile> {
-NamedFile::open("build/index.html")
+  NamedFile::open("build/index.html")
 }
+
 fn rocket() -> rocket::Rocket {
-rocket::ignite()
-.mount(
-"/",
-routes![
-index,
-myrocket,
-files,
-],
-)
-.attach(make_cors())
+  rocket::ignite()
+    .mount(
+      "/",
+      routes![index, myrocket, files,],
+    )
+    .attach(make_cors())
 }
+
 fn main() {
-rocket().launch();
+  rocket().launch();
 }
